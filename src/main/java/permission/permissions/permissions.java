d0dc458251f4551ebde72259539f43906180d36a
5vl <nederlandmax@gmail.com>
2021-01-21 21:01:05 +0100

    1.0.0

--- src/main/java/permission/permissions/permissions.java
+++ src/main/java/permission/permissions/permissions.java
@@ -18,36 +18,28 @@ public final class permissions extends JavaPlugin {
     }
 
     HashMap<UUID, PermissionAttachment> attachments = new HashMap<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("perm")) {
-            Player ps = Bukkit.getPlayer(args[0]);
-            PermissionAttachment attachment = ps.addAttachment(this);
+            PermissionAttachment attachment = Bukkit.getPlayer(args[0]).addAttachment(this);
             Player p = (Player) sender;
-            if (sender instanceof Player) {
-                if (p.hasPermission("perm.setperm")) {
-                    if (args.length > 2) {
-                        attachment.setPermission(args[1], Boolean.parseBoolean(args[2]));
-                    } else {
-                        p.sendMessage("Please specify correct arguments: /perm <player> <permission> <true/false>");
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
+            if (p.hasPermission("perm.perm")) {
+                if (args.length < 3) {
+                    p.sendMessage("Usage: /perm <player> <permission> <true/false>");
+                }
+                else if (args.length == 3) {
+                    attachment.setPermission(args[1], Boolean.parseBoolean(args[2]));
+                    p.sendMessage(ChatColor.WHITE + "Successfully set permission " + ChatColor.YELLOW + args[1] + ChatColor.WHITE + " to " + ChatColor.YELLOW + args[2] + ChatColor.WHITE + " for player " + ChatColor.YELLOW + args[0]);
+                }
+                else {
+                    p.sendMessage("Usage: /perm <player> <permission> <true/false>");
                 }
             }
             else {
-                if (args.length > 2) {
-                    attachment.setPermission(args[1], Boolean.parseBoolean(args[2]));
-                } else {
-                    System.out.println("Please specify correct arguments: /perm <player> <permission> <true/false>");
-                }
+                p.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
             }
         }
         return false;
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
